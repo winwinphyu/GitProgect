6d4ae25bc80f9b33f0e2e5db1622f014b551437f
winwinphyu <wwinphyu78@gmail.com>
2021-11-19 19:58:19 +0630

    second commit

--- app/src/main/java/com/example/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/gitproject/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.gitproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnbutton)
+        button.setOnClickListener{
+            Toast.makeText(this,"Button Click!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+
+
